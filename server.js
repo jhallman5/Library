2dbf0f671fbfb17c963743845e6d4178d382c402
John <jhallman5@gmail.com>
2017-08-07 12:01:55 -0700

    Adds basic views

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const session = require('express-session')
-const viewEngine = require('ejs')
+const path = require('path')
+const { router } = require('./controllers/routes')
 
 const server = express()
 const PORT = process.env.NODE_ENV || 3000
 
-server.set('view engine', viewEngine)
+require('ejs')
+server.set('view engine', 'ejs')
 
-server.use(bodyParser.urlencoded({ extended: false })
+server.use(express.static(path.join(__dirname, 'public')))
+server.use(bodyParser.urlencoded({ extended: false }))
+
+server.use(router)
 
 server.listen(PORT, () => {
   console.log('The server is listening on port ' + PORT)
